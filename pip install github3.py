d5bacfbb09ca5c71b786c7ed179fa87ef47cfc17
python517 <vertx313@gmail.com>
2024-12-21 17:52:15 +0300

    Update pip install github3.py

--- pip install github3.py
+++ pip install github3.py
@@ -10,6 +10,6 @@ git add .                  # Add all files to Git
 git commit -m "Adds repo structure for trojan."   # Commit changes to Git
 
 # Replace <yourusername> with your GitHub username
-git remote add origin https://github.com/<yourusername>/bhptrojan.git   # Link the local repository to your GitHub repository
+git remote add origin https://github.com/<python517>/bhptrojan.git   # Link the local repository to your GitHub repository
 
 git push origin master      # Push changes to GitHub
